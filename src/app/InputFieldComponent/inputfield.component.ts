80e93f040a9afd4d2f40404765abc5082d56c7ff
rajarajankamban <rajarajankamban@users.noreply.github.com>
2017-04-17 16:30:03 +0530

    Form with JSON object

--- src/app/InputFieldComponent/inputfield.component.ts
+++ src/app/InputFieldComponent/inputfield.component.ts
@@ -1,5 +1,5 @@
 import {Component} from "@angular/core";
-
+import {userDetails} from "./userDetails";
 @Component({
     templateUrl:'inputfield.component.html',
     selector: 'input-sample'
@@ -9,6 +9,7 @@ export class InputFieldComponent{
       title = 'app works!';
   product = {name:'product 1'};
   name : string = "";
+
   changeName(value: string){
     console.log("Test :"+value)
     this.name += value + "\n";
@@ -17,4 +18,7 @@ export class InputFieldComponent{
   toggleValue(): void{
     this.showItem = !this.showItem;
   }
+//User details add component
+  user : userDetails[] = [];
+
 } 
---
+++ src/app/InputFieldComponent/userDetails.ts
@@ -0,0 +1,10 @@
+export class userDetails {
+    name : string;
+    gender : string;
+    option : string;
+    constructor(_name: string, _gender: string, _option: string){
+        this.name = _name;
+        this.gender = _gender;
+        this.option = _option;
+    }
+}
